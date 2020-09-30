0696a9c32abc717b1f0ddafe2db6837747f90a4b
kaif vakil <kaifvakil02@gmail.com>
2020-09-30 11:35:16 -0400

    Updated student class

--- Student.cpp
+++ Student.cpp
@@ -6,6 +6,10 @@
 
 #include "Student.h"
 
+#include <iostream>
+#include <string>
+#include <utility>
+
 /**
  * Student implementation
  */
@@ -17,34 +21,47 @@
  * @param age
  * @param student_id
  */
-void Student::Student(string first_name, string last_name, float age, string student_id) {
+Student::Student(string first_name, string last_name, float age, string student_id)
+    :Person(std::move(first_name), std::move(last_name), age), student_id_(std::move(student_id))
+{
 
 }
 
 /**
  * @return string
  */
-string Student::getStudentID() {
-    return "";
+string Student::getStudentID() const
+{
+    return m_studentID;
 }
 
 /**
  * @param value
  */
-void Student::setStudentID(string value) {
-
+void Student::setStudentID(string value)
+{
+    m_studentID = std::move(value);
 }
 
 /**
  * @return void
  */
-void Student::Studies() {
-    return;
+void Student::Studies()
+{
+    cout << getFirstName() << " is studying" << endl;
 }
 
 /**
  * @return string
  */
-string Student::ToString() {
-    return "";
+string Student::ToString()
+{
+    std::string output_string;
+
+    output_string += Person::ToString();
+    output_string += "----------------------------------\n";
+    output_string += "First name: " + getStudentID() + "\n";
+    output_string += "----------------------------------\n";
+
+    return output_string;
 }
--- Student.h
+++ Student.h
@@ -3,11 +3,13 @@
  * @author Kaifkhan Vakil
  */
 
+#pragma once
 
 #ifndef _STUDENT_H
 #define _STUDENT_H
 
 #include "Person.h"
+#include <string>
 
 
 class Student: public Person {
@@ -19,9 +21,9 @@ public:
  * @param age
  * @param student_id
  */
-void Student(string first_name, string last_name, float age, string student_id);
+ Student(string first_name, string last_name, float age, string student_id);
     
-string getStudentID();
+string getStudentID() const;
     
 /**
  * @param value
@@ -30,9 +32,10 @@ void setStudentID(string value);
     
 void Studies();
     
-string ToString();
+string ToString() override;
 private: 
     string m_studentID;
+
 };
 
 #endif //_STUDENT_H
